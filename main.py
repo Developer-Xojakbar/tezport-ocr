6bc5730b8e54a872a1460318028ee54fcf036c44
Xojakbar <b.xojakbar.b@gmail.com>
2025-12-25 10:41:20 +0500

    render deploy codes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import os
 import uvicorn
 
 from fastapi import FastAPI, File, UploadFile
@@ -47,5 +48,6 @@ async def ocr_image(image: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
